e1177f9588ba6f3a08acbbd304f0836a9c4ec710
Ritika Singh <ritikasingh120705@gmail.com>
2026-02-24 07:35:40 +0000

    Day4 changes (in App component)

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -4,32 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <h1>Hi I am Ritika Singh </h1>
+    <h1>Inside the app component </h1>
+    <Employee></Employee>
+    <Sum></Sum>
+    </div>
+  
   )
+}
+function Employee(){
+  return(
+    <h1>The employee name is Ritika Singh</h1>
+  )
+}
+function Sum(){
+  return(10+10);
 }
 
 export default App
